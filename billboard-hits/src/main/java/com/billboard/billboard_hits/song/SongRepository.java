5f027bb28c3f633ad58e69e3aa83df1e91388094
Sekwati <sekwati95@gmail.com>
2025-04-10 17:29:33 +0200

    Fix: Corrected primary key type in SongRepository from String to Integer

--- billboard-hits/src/main/java/com/billboard/billboard_hits/song/SongRepository.java
+++ billboard-hits/src/main/java/com/billboard/billboard_hits/song/SongRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SongRepository extends JpaRepository<Song,String>{
+public interface SongRepository extends JpaRepository<Song,Integer>{
 	
 	void deleteBySongName(String songName);
 	
